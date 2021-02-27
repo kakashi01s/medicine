ba62d3e29028eacd2a3218aec4f551deddc27907
ayushrana <ayush@getzyme.com>
2021-02-27 22:59:48 +0530

    location fixed

--- app/src/main/java/food/delivery/fantasy/hub/online/coupons/deals/network/bazaar/tracker/view/WebActivity.kt
+++ app/src/main/java/food/delivery/fantasy/hub/online/coupons/deals/network/bazaar/tracker/view/WebActivity.kt
@@ -1,6 +1,10 @@
 package food.delivery.fantasy.hub.online.coupons.deals.network.bazaar.tracker.view
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
@@ -12,6 +16,8 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.facebook.ads.*
@@ -42,6 +48,11 @@ class WebActivity : AppCompatActivity() {
 
     var firebaseRemoteConfig: FirebaseRemoteConfig? = null
 
+    var mGeoLocationRequestOrigin: String? = null
+    var mGeoLocationCallback: GeolocationPermissions.Callback? = null
+
+    private val LOCATION_PERMISSION_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
@@ -57,6 +68,9 @@ class WebActivity : AppCompatActivity() {
             onLoadFbInterstitial()
         }
         loadWebSplash()
+
+        ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_CODE)
 
         webViewSettings()
 
@@ -118,6 +132,54 @@ class WebActivity : AppCompatActivity() {
         }
 
         webView!!.webChromeClient = object : WebChromeClient(){
+
+            override fun onGeolocationPermissionsShowPrompt(
+                    origin: String?,
+                    callback: GeolocationPermissions.Callback?
+            ) {
+                if (ContextCompat.checkSelfPermission(
+                                this@WebActivity,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                        )
+                        != PackageManager.PERMISSION_GRANTED
+                ) {
+
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                                    this@WebActivity,
+                                    Manifest.permission.ACCESS_FINE_LOCATION
+                            )
+                    ) {
+                        AlertDialog.Builder(this@WebActivity)
+                                .setMessage("Please turn ON the GPS to make app work smoothly")
+                                .setNeutralButton(
+                                        android.R.string.ok,
+                                        DialogInterface.OnClickListener { dialogInterface, i ->
+                                            mGeoLocationCallback = callback
+                                            mGeoLocationRequestOrigin = origin
+                                            ActivityCompat.requestPermissions(
+                                                    this@WebActivity,
+                                                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1001
+                                            )
+
+                                        })
+                                .show()
+
+                    } else {
+                        //no explanation need we can request the locatio
+                        mGeoLocationCallback = callback
+                        mGeoLocationRequestOrigin = origin
+                        ActivityCompat.requestPermissions(
+                                this@WebActivity,
+                                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1001
+                        )
+                    }
+                } else {
+                    //tell the webview that permission has granted
+                    callback!!.invoke(origin, true, true)
+                }
+            }
+
+
             override fun onProgressChanged(view: WebView?, newProgress: Int) {
                 super.onProgressChanged(view, newProgress)
                 if(newProgress >= 80){
@@ -147,6 +209,28 @@ class WebActivity : AppCompatActivity() {
                 mainContainer.visibility = View.VISIBLE
             }
         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            1001 -> {
+                //if permission is cancel result array would be empty
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission was granted
+                    if (mGeoLocationCallback != null) {
+                        mGeoLocationCallback!!.invoke(mGeoLocationRequestOrigin, true, true)
+                    }
+                } else {
+                    //permission denied
+                    if (mGeoLocationCallback != null) {
+                        mGeoLocationCallback!!.invoke(mGeoLocationRequestOrigin, false, false)
+                    }
+                }
+            }
+        }
+
     }
 
     fun loadWebSplash(){
